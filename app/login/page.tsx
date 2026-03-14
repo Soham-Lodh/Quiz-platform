40acca61015cd30467238a235dee538fe1efb494
Soham Lodh <sohamlodh06@gmail.com>
2026-03-15 00:12:13 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginCard from "../../components/LoginCard";
 
 export default function PlayerLogin() {
   return (
-    <div className="flex justify-center items-center h-full bg-white">
+    <div className="flex justify-center items-center h-full bg-[#e5e5e5]">
       <LoginCard
         bgColour="#AEEBFF"
         title="Welcome back, user!"
